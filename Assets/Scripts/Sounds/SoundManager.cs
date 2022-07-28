7260ba8a31a9b91ba2208d5d83f2c01d26aeff5e
praksahb <39479839+praksahb@users.noreply.github.com>
2022-07-28 21:49:15 +0530

    singleton sound manager added

---
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager Instance { get { return instance; } }
+
+    public AudioSource soundEffect;
+    public AudioSource soundMusic;
+
+    public SoundType[] SoundsArray;
+
+    private void Awake()
+    {
+        CreateLevelManager();
+    }
+
+    private void Start()
+    {
+        PlayMusic(Sounds.Music);
+    }
+
+    private void CreateLevelManager()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void PlayMusic(Sounds sound)
+    {
+        AudioClip clip = GetSoundClip(sound);
+        if (clip != null)
+        {
+            soundMusic.clip = clip;
+            soundMusic.Play();
+        }
+        else
+        {
+            Debug.LogError("Clip not found for sound type: " + sound);
+        }
+    }
+
+    public void PlayEffect(Sounds sound)
+    {
+        AudioClip clip = GetSoundClip(sound);
+        if(clip != null)
+        {
+            soundEffect.PlayOneShot(clip);
+        } else
+        {
+            Debug.LogError("Clip not found for sound type: " + sound);
+        }
+    }
+
+    private AudioClip GetSoundClip(Sounds sound)
+    {
+        SoundType audioItem = Array.Find(SoundsArray, item => item.soundType == sound);
+        if(audioItem != null)
+            return audioItem.soundClip;
+        return null;
+    }
+}
+
+[Serializable]
+public class SoundType
+{
+    public Sounds soundType;
+    public AudioClip soundClip;
+}
+
+public enum Sounds
+{
+    ButtonClick,
+    Music,
+    PlayerMove,
+    PlayerDeath,
+    EnemyDeath
+}
