fa97d75d166ae6d93b01cff5b4d87f732b7397b0
praksahb <39479839+praksahb@users.noreply.github.com>
2022-08-01 14:57:01 +0530

    added a timer function will expand on later
    can be used to calculate
    - time taken to complete level

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float countTimer;
+    public bool IsTimerRunning = false;
+
+    public Text timeText;   
+
+    private void Update()
+    {
+        if(IsTimerRunning)
+        {
+            RunCountTimer();
+            DisplayTime(countTimer);
+        }
+    }
+
+    private void RunCountTimer()
+    {
+        countTimer += Time.deltaTime;
+    }
+
+    public void DisplayTime(float displayTime)
+    {
+        float minutes = Mathf.FloorToInt(displayTime / 60);
+        float seconds = Mathf.FloorToInt(displayTime % 60);
+        float milliSeconds = (displayTime % 1) * 1000;
+        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
+    }
+}
