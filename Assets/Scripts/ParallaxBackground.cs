17811bc49612f8169b3bb6afbcb2cd2a64ce70a1
praksahb <39479839+praksahb@users.noreply.github.com>
2022-08-16 00:32:13 +0530

    wrap up parallax
    parallax will appear better with images with depth as well

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
@@ -8,7 +6,43 @@ public class ParallaxBackground : MonoBehaviour
     private Vector3 cameraLastPosition;
     public Vector2 parallaxFactor;
 
+    private Vector2 backgroundParallaxFactor;
+    private Vector2 midgroundParallaxFactor;
+    private Vector2 cloudParallaxFactor;
+
     //public CameraController cameraController;
+
+    void Awake()
+    {
+        SetParallaxFactorAccordingToLayerOrder();
+    }
+
+    private void SetParallaxFactorAccordingToLayerOrder()
+    {
+        switch (gameObject.layer)
+        {
+            case 20:
+                //will follow camera
+                parallaxFactor.Set(1f, 1f);
+                break;
+            case 12:
+                //clouds
+                parallaxFactor.Set(0.8f, 0.4f);
+                break;
+            case 10:
+                //distant mountains
+                parallaxFactor.Set(0.4f, 0.3f);
+                break;
+            case 9:
+                //Foreground move ahead of camera
+                parallaxFactor.Set(-0.4f, 0f);
+                break;
+            default:
+                //no parallax effect
+                parallaxFactor.Set(0f, 0f);
+                break;
+        }
+    }
 
     void Start()
     {
@@ -18,12 +52,13 @@ public class ParallaxBackground : MonoBehaviour
 
     void Update()
     {
-        //ApplyDeltaValues();
+        MoveGameObject();
+
     }
 
     void LateUpdate()
     {
-        MoveGameObject();
+        //MoveGameObject();
     }
 
     private void MoveGameObject()
@@ -33,21 +68,20 @@ public class ParallaxBackground : MonoBehaviour
         cameraLastPosition = cameraTransform.position;
     }
 
-    //modify transform values of gameObject
-    private void ApplyDeltaValues()
-    {
-        Vector3 position = transform.position;
-        position += ModifyDeltaValues(PlayerController.PlayerDeltaMovement);
-        transform.position = position;
-    }
+    ////modify transform values of gameObject
+    //private void ApplyDeltaValues()
+    //{
+    //    Vector3 position = transform.position;
+    //    position += ModifyDeltaValues(PlayerController.PlayerDeltaMovement);
+    //    transform.position = position;
+    //}
 
-    Vector3 ModifyDeltaValues(Vector3 delta)
-    {
-        //delta = PlayerController.PlayerDeltaMovement;
-        delta.x *= parallaxFactor.x;
-        delta.y *= parallaxFactor.y;
-        return delta;
-    }
-
+    //Vector3 ModifyDeltaValues(Vector3 delta)
+    //{
+    //    //delta = PlayerController.PlayerDeltaMovement;
+    //    delta.x *= parallaxFactor.x;
+    //    delta.y *= parallaxFactor.y;
+    //    return delta;
+    //}
 
 }
